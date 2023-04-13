0f6a4d4f32ce4f9695e288dd400b851e769eb8ca
MysteriousDeve <flashlight135790@gmail.com>
2023-04-13 16:30:54 +0700

    a

--- WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1.cpp
@@ -47,6 +47,8 @@ Gdiplus::Image* img;
 GdiplusStartupInput startInput;
 ULONG_PTR gdiToken;
 
+BITMAP backbuffer;
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLine, int nCmdShow)
 {
     GdiplusStartup(&gdiToken, &startInput, NULL);
@@ -123,6 +125,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
             {
                 timer = hrc::now();
 
+                // Handle window dragging
                 if (mouseIsDown == 1)
                 {
                     POINT cPos;
@@ -142,115 +145,193 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLin
     return 0;
 }
 
+void Paint(HWND hWnd, LPPAINTSTRUCT lpPS);
 LRESULT CALLBACK WinProc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
 {
     switch (Message)
     {
-    case WM_CREATE:
-    {
-        RECT wndRect;
-        GetWindowRect(hWnd, &wndRect);
-
-        flybarRect = wndRect;
-        wndRect.bottom = flybarSize;
-
-        hFont = CreateFontA(
-            20,
-            0,
-            0,
-            0,
-            FW_NORMAL,
-            FALSE,
-            FALSE,
-            FALSE,
-            DEFAULT_CHARSET,
-            OUT_OUTLINE_PRECIS,
-            CLIP_DEFAULT_PRECIS,
-            CLEARTYPE_QUALITY,
-            VARIABLE_PITCH,
-            "Segoe UI"
-        );
-    }
-
-    case WM_LBUTTONDOWN:
-    {
-        if (mouseIsDown == -1)
+        case WM_CREATE:
         {
-            mouseIsDown = 0;
+            RECT wndRect;
+            GetWindowRect(hWnd, &wndRect);
+
+            backbuffer = CreateBitmap(wndRect.right - wndRect.left, wndRect.bottom - wndRect.top, );
+
+            flybarRect = wndRect;
+            wndRect.bottom = flybarSize;
+
+            hFont = CreateFontA(
+                20,
+                0,
+                0,
+                0,
+                FW_NORMAL,
+                FALSE,
+                FALSE,
+                FALSE,
+                DEFAULT_CHARSET,
+                OUT_OUTLINE_PRECIS,
+                CLIP_DEFAULT_PRECIS,
+                CLEARTYPE_QUALITY,
+                VARIABLE_PITCH,
+                "Segoe UI"
+            );
+        }
+
+        case WM_LBUTTONDOWN:
+        {
+            if (mouseIsDown == -1)
+            {
+                mouseIsDown = 0;
+                return 0;
+            }
+            RECT wndRect;
+            GetWindowRect(hWnd, &wndRect);
+
+            POINT cPos;
+            GetCursorPos(&cPos);
+
+            if (PtInRect(&wndRect, cPos))
+            {
+                wndMouseDragOffset.x = wndRect.left - cPos.x;
+                wndMouseDragOffset.y = wndRect.top - cPos.y;
+                mouseIsDown = 1;
+            }
             return 0;
         }
-        RECT wndRect;
-        GetWindowRect(hWnd, &wndRect);
 
-        POINT cPos;
-        GetCursorPos(&cPos);
-
-        if (PtInRect(&wndRect, cPos))
+        case WM_LBUTTONUP:
         {
-            wndMouseDragOffset.x = wndRect.left - cPos.x;
-            wndMouseDragOffset.y = wndRect.top - cPos.y;
-            mouseIsDown = 1;
+            mouseIsDown = false;
+            return 0;
+        }
+
+        case WM_ERASEBKGND: return (LRESULT)1; // Tell Windows we handle the background erase, to prevent flickering ig...
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            SelectObject(hdc, GetStockObject(DC_BRUSH));
+            RECT wrect, textRect;
+            GetWindowRect(hWnd, &wrect);
+
+            Paint(hWnd, &ps);
+
+
+            Gdiplus::Graphics g(hdc);
+            g.Clear(Gdiplus::Color(20, 0, 0, 0));
+            g.SetInterpolationMode(InterpolationModeNearestNeighbor);
+            g.SetPixelOffsetMode(PixelOffsetModeNone);
+            Rect gdirect(0, 0, 200, 200);
+            g.DrawImage(img, gdirect);
+
+
+            EndPaint(hWnd, &ps);
         }
         return 0;
-    }
 
-    case WM_LBUTTONUP:
-    {
-        mouseIsDown = false;
-        return 0;
-    }
-
-    case WM_MOUSEMOVE:
-    {
-
-    }
-
-    case WM_ERASEBKGND:
-        return (LRESULT)1; // Tell Windows we handle the background erase, to prevent flickering ig...
-
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        SelectObject(hdc, GetStockObject(DC_BRUSH));
-        RECT wrect, textRect;
-        GetWindowRect(hWnd, &wrect);
-
-        FillRect(hdc, &ps.rcPaint, bkgClr);
-
-
-        RECT flybarRect;
-        flybarRect.right = wrect.right - wrect.left;
-        flybarRect.bottom = flybarSize;
-        SetRect(&textRect, 10, 0, wrect.right - 10, flybarSize);
-        FillRect(hdc, &flybarRect, flybarClr);
-
-
-        LPCWSTR msg = L"Hello";
-        SelectObject(hdc, hFont);
-        SetTextColor(hdc, RGB(255, 255, 255));
-        SetBkMode(hdc, TRANSPARENT);
-        DrawText(hdc, msg, -1, &textRect, DT_SINGLELINE | DT_MODIFYSTRING | DT_VCENTER);
-
-
-        Gdiplus::Graphics g(hdc);
-        g.Clear(Gdiplus::Color(20, 0, 0, 0));
-        g.SetInterpolationMode(InterpolationModeNearestNeighbor);
-        g.SetPixelOffsetMode(PixelOffsetModeNone);
-        Rect gdirect(0, 0, 200, 200);
-        g.DrawImage(img, gdirect);
-
-
-        EndPaint(hWnd, &ps);
-    }
-    return 0;
-
-    case WM_DESTROY:
-    {
-        GdiplusShutdown(gdiToken);
-        PostQuitMessage(0);
-        return 0;
-    }
+        case WM_DESTROY:
+        {
+            GdiplusShutdown(gdiToken);
+            PostQuitMessage(0);
+            return 0;
+        }
     }
     return DefWindowProc(hWnd, Message, wParam, lParam);
 }
+
+
+
+
+void Paint(HWND hWnd, LPPAINTSTRUCT lpPS)
+{
+    RECT rc;
+    HDC hdcMem;
+    HBITMAP hbmMem, hbmOld;
+    HBRUSH hbrBkGnd;
+    HFONT hfntOld;
+
+    // Get the size of the client rectangle.
+    GetClientRect(hWnd, &rc);
+
+    // Create a compatible DC.
+    hdcMem = CreateCompatibleDC(lpPS->hdc);
+
+    // Create a bitmap big enough for our client rectangle.
+    hbmMem = CreateCompatibleBitmap(lpPS->hdc,
+        rc.right - rc.left,
+        rc.bottom - rc.top);
+
+    // Select the bitmap into the off-screen DC.
+    hbmOld = SelectObject(hdcMem, hbmMem);
+
+    // Erase the background.
+    hbrBkGnd = CreateSolidBrush(GetSysColor(COLOR_WINDOW));
+    FillRect(hdcMem, &rc, hbrBkGnd);
+    DeleteObject(hbrBkGnd);
+
+    // Select the font.
+    if (hfnt) {
+        hfntOld = SelectObject(hdcMem, hfnt);
+    }
+
+    // Render the image into the offscreen DC.
+    SetBkMode(hdcMem, TRANSPARENT);
+    SetTextColor(hdcMem, GetSysColor(COLOR_WINDOWTEXT));
+    DrawText(hdcMem,
+        szCaption,
+        -1,
+        &rc,
+        DT_CENTER);
+
+    if (hfntOld) {
+        SelectObject(hdcMem, hfntOld);
+    }
+
+    // Blt the changes to the screen DC.
+    BitBlt(lpPS->hdc,
+        rc.left, rc.top,
+        rc.right - rc.left, rc.bottom - rc.top,
+        hdcMem,
+        0, 0,
+        SRCCOPY);
+
+    // Done with off-screen bitmap and DC.
+    SelectObject(hdcMem, hbmOld);
+    DeleteObject(hbmMem);
+    DeleteDC(hdcMem);
+}
+
+
+
+
+
+
+
+
+/*
+ * Unused code
+  
+  
+  
+ 
+ 
+
+            FillRect(hdc, &ps.rcPaint, bkgClr);
+
+
+            RECT flybarRect;
+            flybarRect.right = wrect.right - wrect.left;
+            flybarRect.bottom = flybarSize;
+            SetRect(&textRect, 10, 0, wrect.right - 10, flybarSize);
+            FillRect(hdc, &flybarRect, flybarClr);
+
+
+            LPCWSTR msg = L"Hello";
+            SelectObject(hdc, hFont);
+            SetTextColor(hdc, RGB(255, 255, 255));
+            SetBkMode(hdc, TRANSPARENT);
+            DrawText(hdc, msg, -1, &textRect, DT_SINGLELINE | DT_MODIFYSTRING | DT_VCENTER);
+
+*/
